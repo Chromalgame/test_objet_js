d3a6d07fa2dc03efbbb62e78c180086601460dfb
Chromalgame <mattbubu@free.fr>
2021-07-30 14:16:33 +0200

    init

--- app.js
+++ app.js
@@ -51,30 +51,55 @@
 
 import {player} from "./script.js";
 
-const div = document.createElement('div');
-div.className = `${player.getName()}`;
-div.style.position = `absolute`;
-div.style.backgroundImage = `url(${player.getSkin()})`;
-div.style.backgroundSize = `cover`;
-div.style.width = `50px`;
-div.style.height = `50px`;
+const div_content_player = document.createElement('div');
+div_content_player.className = `${player.getName()}`;
+div_content_player.style.position = `absolute`;
+div_content_player.style.width = `7%`;
+div_content_player.style.height = `auto`;
+div_content_player.style.display = `flex`;
+div_content_player.style.flexDirection = `column`;
 
-document.querySelector('body').appendChild(div);
+const div_player = document.createElement('div');
+div_player.position = 'relative';
+div_player.style.backgroundSize = `cover`;
+div_player.style.width = `100%`;
+div_player.style.height = `auto`;
+
+const div_player_skin = document.createElement('img');
+div_player_skin.src = `${player.getSkin()}`;
+div_player_skin.position = 'absolute';
+div_player_skin.style.top = '0';
+div_player_skin.style.left = '0';
+div_player_skin.style.width = '100%';
+div_player_skin.style.marginTop = '5%';
+
+const div_nom = document.createElement('div');
+div_nom.style.backgroundSize = `cover`;
+div_nom.style.fontSize = '15px';
+div_nom.style.color = "#f1f1f1";
+div_nom.style.width = `100%`;
+div_nom.style.textAlign = `center`;
+div_nom.innerText = player.getName();
+
+document.querySelector('body').appendChild(div_content_player);
+div_content_player.appendChild(div_nom);
+div_content_player.appendChild(div_player);
+div_player.appendChild(div_player_skin);
 
 document.addEventListener('keydown', logKey);
 
 function logKey(e) {
   const px_move = 10;
   if(e.code == 'ArrowUp'){
-    div.style.top = `${div.offsetTop - px_move}px`;
+    div_content_player.style.top = `${div_content_player.offsetTop - px_move}px`;
   }
   if(e.code == 'ArrowDown'){
-    div.style.top = `${div.offsetTop + px_move}px`;
+    div_content_player.style.top = `${div_content_player.offsetTop + px_move}px`;
   }
   if(e.code == 'ArrowLeft'){
-    div.style.left = `${div.offsetLeft - px_move}px`;
+    div_content_player.style.left = `${div_content_player.offsetLeft - px_move}px`;
   }
   if(e.code == 'ArrowRight'){
-    div.style.left = `${div.offsetLeft + px_move}px`;
+    div_content_player.style.left = `${div_content_player.offsetLeft + px_move}px`;
   }
 }
--- script.js
+++ script.js
@@ -11,46 +11,46 @@ const rock = new Spell("Rock", -10, 10, 1, 1);
 // Défensif
 const heal = new Spell("Heal", +10, 10, 1, 1);
 
-console.log(``);
+// console.log(``);
 
-console.log(`----- SPELL -----`);
-console.log(`Name : ${fire.getName()}`);
-console.log(`Damage : ${fire.getDamage()}`);
-console.log(`Mana_Cost : ${fire.getMana_Cost()}`);
-console.log(`CoolDown : ${fire.getCooldown()}`);
-console.log(`LVL : ${fire.getLVL()}`);
+// console.log(`----- SPELL -----`);
+// console.log(`Name : ${fire.getName()}`);
+// console.log(`Damage : ${fire.getDamage()}`);
+// console.log(`Mana_Cost : ${fire.getMana_Cost()}`);
+// console.log(`CoolDown : ${fire.getCooldown()}`);
+// console.log(`LVL : ${fire.getLVL()}`);
 
-console.log(``);
+// console.log(``);
 
-console.log(`Name : ${ice.getName()}`);
-console.log(`Damage : ${ice.getDamage()}`);
-console.log(`Mana_Cost : ${ice.getMana_Cost()}`);
-console.log(`CoolDown : ${ice.getCooldown()}`);
-console.log(`LVL : ${ice.getLVL()}`);
+// console.log(`Name : ${ice.getName()}`);
+// console.log(`Damage : ${ice.getDamage()}`);
+// console.log(`Mana_Cost : ${ice.getMana_Cost()}`);
+// console.log(`CoolDown : ${ice.getCooldown()}`);
+// console.log(`LVL : ${ice.getLVL()}`);
 
-console.log(``);
+// console.log(``);
 
-console.log(`Name : ${storm.getName()}`);
-console.log(`Damage : ${storm.getDamage()}`);
-console.log(`Mana_Cost : ${storm.getMana_Cost()}`);
-console.log(`CoolDown : ${storm.getCooldown()}`);
-console.log(`LVL : ${storm.getLVL()}`);
+// console.log(`Name : ${storm.getName()}`);
+// console.log(`Damage : ${storm.getDamage()}`);
+// console.log(`Mana_Cost : ${storm.getMana_Cost()}`);
+// console.log(`CoolDown : ${storm.getCooldown()}`);
+// console.log(`LVL : ${storm.getLVL()}`);
 
-console.log(``);
+// console.log(``);
 
-console.log(`Name : ${rock.getName()}`);
-console.log(`Damage : ${rock.getDamage()}`);
-console.log(`Mana_Cost : ${rock.getMana_Cost()}`);
-console.log(`CoolDown : ${rock.getCooldown()}`);
-console.log(`LVL : ${rock.getLVL()}`);
+// console.log(`Name : ${rock.getName()}`);
+// console.log(`Damage : ${rock.getDamage()}`);
+// console.log(`Mana_Cost : ${rock.getMana_Cost()}`);
+// console.log(`CoolDown : ${rock.getCooldown()}`);
+// console.log(`LVL : ${rock.getLVL()}`);
 
-console.log(``);
+// console.log(``);
 
-console.log(`Name : ${heal.getName()}`);
-console.log(`Damage : ${heal.getDamage()}`);
-console.log(`Mana_Cost : ${heal.getMana_Cost()}`);
-console.log(`CoolDown : ${heal.getCooldown()}`);
-console.log(`LVL : ${heal.getLVL()}`);
+// console.log(`Name : ${heal.getName()}`);
+// console.log(`Damage : ${heal.getDamage()}`);
+// console.log(`Mana_Cost : ${heal.getMana_Cost()}`);
+// console.log(`CoolDown : ${heal.getCooldown()}`);
+// console.log(`LVL : ${heal.getLVL()}`);
 
 /********************** PLAYER **********************/
 
@@ -62,12 +62,12 @@ player.addSpell(storm);
 player.addSpell(rock);
 player.addSpell(heal);player.addSpell(heal);
 
-console.log(`---- PLAYER ----`);
-console.log(`Name : ${player.getName()}`);
-console.log(`Skin : ${player.getSkin()}`);
-console.log(`Health : ${player.getHealth()}`);
-console.log(`Armor : ${player.getArmor()}`);
-console.log(`Mana : ${player.getMana_Count()}`);
-console.log(`LVL : ${player.getLVL()}`);
-console.log(`Spell :`);
-player.getSpell().forEach((spell) => {console.log(" - ", spell.getName());})
+// console.log(`---- PLAYER ----`);
+// console.log(`Name : ${player.getName()}`);
+// console.log(`Skin : ${player.getSkin()}`);
+// console.log(`Health : ${player.getHealth()}`);
+// console.log(`Armor : ${player.getArmor()}`);
+// console.log(`Mana : ${player.getMana_Count()}`);
+// console.log(`LVL : ${player.getLVL()}`);
+// console.log(`Spell :`);
+// player.getSpell().forEach((spell) => {console.log(" - ", spell.getName());})
